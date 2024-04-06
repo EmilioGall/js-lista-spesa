fd25d1793e78faab446043bbd3eb88258d01641d
Emilio Gallo <emilio.gallo17@gmail.com>
2024-04-06 13:12:51 +0200

    add: input item behaviour - html&js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const shoppingList = ["pane", "pasta", "frutta", "detersivo", "dentifricio"]
+const shoppingList = ["bread", "pasta", "pears", "tomatoes", "cabbage"];
 
 console.log("ShoppingList", shoppingList);
 
@@ -15,9 +15,10 @@ while (i < shoppingList.length) {
 
     console.log(curShoppingItem);
 
+    // Print List on HTML - START
     const listItemElem = document.createElement("li"); // object
 
-    listItemElem.classList.add("list-group-item")
+    listItemElem.classList.add("list-group-item");
 
     listItemElem.innerHTML = curShoppingItem;
 
@@ -30,7 +31,43 @@ while (i < shoppingList.length) {
     });
 
     shoppingListElem.append(listItemElem);
+    // Print List on HTML - END
 
     i++
 
 };
+
+// New Item Input Form
+
+let newListItemInput = document.getElementById("new-list-item");
+
+const inputForm = document.getElementById("shopping-form");
+
+inputForm.addEventListener ("submit", function(e) {
+
+    e.preventDefault() // Prevent default behaviour of form tag
+
+    const newListItem = newListItemInput.value;
+
+    shoppingList.push(newListItem)
+
+    // Print List on HTML - START
+    const listItemElem = document.createElement("li"); // object
+
+    listItemElem.classList.add("list-group-item");
+
+    listItemElem.innerHTML = newListItem;
+
+    console.log(listItemElem);
+
+    listItemElem.addEventListener("click", function() {
+
+        listItemElem.classList.toggle("active");
+
+    });
+
+    shoppingListElem.append(listItemElem);
+    // Print List on HTML - END
+
+});
+
